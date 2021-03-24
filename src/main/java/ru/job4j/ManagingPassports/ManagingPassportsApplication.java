e95f577f1e1580f6af5ea9814d663232b7050c37
dmitrytishchenko <tdmitiu@mail.ru>
2021-03-24 19:22:47 +0300

    first commit - start

---
+++ src/main/java/ru/job4j/ManagingPassports/ManagingPassportsApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.ManagingPassports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingPassportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingPassportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/ManagingPassports/ManagingPassportsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.ManagingPassports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingPassportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
